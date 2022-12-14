71524dd3ecd4c29713fd672cae0e51e203bb4682
Yang Hao-Wei <w95wayne10@gmail.com>
2022-12-14 20:31:15 +0800

    add env variable tuner for model parameter
    string to number reference: https://www.freecodecamp.org/news/python-convert-string-to-int-how-to-cast-a-string-in-python/

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -11,12 +11,12 @@ class ChatGPT:
 
     def get_response(self):
         response = openai.Completion.create(
-        model="text-davinci-003", 
-        prompt=self.prompt.generate_prompt(),
-        temperature=0,
-        frequency_penalty = 0,
-        presence_penalty = 0.6,
-        max_tokens=240
+        model = os.getenv("OPENAI_MODEL",default="text-davinci-003"), 
+        prompt = self.prompt.generate_prompt(),
+        temperature = int(os.getenv("OPENAI_TEMPERATURE",default="0")),
+        frequency_penalty = int(os.getenv("OPENAI_FREQUENCY_PENALTY",default="0")),
+        presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY",default="0.6")),
+        max_tokens = int(os.getenv("OPENAI_MAX_TOKENS",default="240"))
         )
         return response['choices'][0]['text'].strip()
 
