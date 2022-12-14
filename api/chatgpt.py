905593f06167a599581676f254096bede9cb5cca
howard <howarder3@gmail.com>
2022-12-14 19:22:30 +0800

    add env var MAX_TOKENS

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -8,6 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
+        self.max_tokens = int(os.getenv("MAX_TOKENS", default = 240))
 
     def get_response(self):
         response = openai.Completion.create(
@@ -16,7 +17,7 @@ class ChatGPT:
         temperature=0,
         frequency_penalty = 0,
         presence_penalty = 0.6,
-        max_tokens=240
+        max_tokens = self.max_tokens
         )
         return response['choices'][0]['text'].strip()
 
