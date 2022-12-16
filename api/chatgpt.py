a86c4611672321339caafef9ded91b0d2f27076a
Yang Hao-Wei <w95wayne10@gmail.com>
2022-12-16 15:44:17 +0000

    deal with word count limit case

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -24,7 +24,7 @@ class ChatGPT:
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        return response['choices'][0]['text'].strip()
+        return response['choices'][0]['text'].strip(), response['choices'][0]['finish_reason']
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
